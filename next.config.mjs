1aae1317b7e8044a33eb9e012abbd81369db441f
o-Bunny-o <pjudithoh@gmail.com>
2025-03-07 22:29:43 -0500

    trying to export the styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/calculator", 
-    assetPrefix: "/calculator/",    
-    trailingSlash: true,
+   // basePath: "/calculator", 
+   // assetPrefix: "/calculator/",    
+   // trailingSlash: true,
 };
 
 export default nextConfig;
